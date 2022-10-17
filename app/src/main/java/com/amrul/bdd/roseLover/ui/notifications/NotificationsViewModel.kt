d8184025a8cc724c5c71fab792ff3d4ac3376b18
chairul <chairul.amri@ebconnect.com>
2022-10-17 19:28:26 +0700

    init project

---
+++ app/src/main/java/com/amrul/bdd/roseLover/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.amrul.bdd.roseLover.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
