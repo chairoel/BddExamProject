d363e594c427a6f0b9f20af76c95a12ec1c8af71
chairul <chairul.amri@ebconnect.com>
2022-10-19 12:30:05 +0700

    add style and prepare to Parcelize for move with data model

--- app/src/main/java/com/amrul/bdd/roseLover/data/Rose.kt
+++ app/src/main/java/com/amrul/bdd/roseLover/data/Rose.kt
@@ -1,9 +1,12 @@
 package com.amrul.bdd.roseLover.data
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Rose(
     var name: String = "",
     var detail: String = "",
     var color: String = "",
     var photo: Int = 0,
-
-)
+) : Parcelable
--- app/src/main/java/com/amrul/bdd/roseLover/util/Constant.kt
+++ app/src/main/java/com/amrul/bdd/roseLover/util/Constant.kt
@@ -4,4 +4,6 @@ object Constant {
     const val ROSE_NAME = "rose_name"
     const val ROSE_DETAIL = "rose_detail"
     const val ROSE_COLOR = "rose_color"
+    const val ROSE_IMAGE = "rose_image"
+    const val ROSE_ITEM = "rose_item"
 }
