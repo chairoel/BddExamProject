80d407cd911e0d9544b497293353c32a0afc5ec4
chairul <chairul.amri@ebconnect.com>
2022-10-21 21:16:47 +0700

    set knowledge UI and data

--- app/src/main/java/com/amrul/bdd/roseLover/ui/fragment/knowledge/KnowledgeFragment.kt
+++ app/src/main/java/com/amrul/bdd/roseLover/ui/fragment/knowledge/KnowledgeFragment.kt
@@ -1,13 +1,22 @@
 package com.amrul.bdd.roseLover.ui.fragment.knowledge
 
 import android.os.Bundle
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
+import android.view.SurfaceControl.Transaction
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.cardview.widget.CardView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
+import com.amrul.bdd.roseLover.R
 import com.amrul.bdd.roseLover.databinding.FragmentKnowledgeBinding
+import kotlinx.android.synthetic.main.fragment_knowledge.*
+import kotlin.properties.Delegates
 
 class KnowledgeFragment : Fragment() {
 
@@ -32,7 +41,31 @@ class KnowledgeFragment : Fragment() {
 //        knowledgeViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
+        binding.tvTitleKnowledge.setOnClickListener {
+            expandedView(binding.expandableLayout, binding.cvKnowledge, binding.ivArrow)
+        }
+
+        binding.tvTitleKnowledge2.setOnClickListener {
+            expandedView(binding.expandableLayout2, binding.cvKnowledge2, binding.ivArrow2)
+        }
+
         return root
+    }
+
+    private fun expandedView(
+        expandableLayout: LinearLayout,
+        cvKnowledge: CardView,
+        ivArrow: ImageView
+    ) {
+        if (expandableLayout.visibility == View.VISIBLE) {
+            expandableLayout.visibility = View.GONE
+            ivArrow.setImageResource(R.drawable.ic_arrow_down_24)
+        } else {
+            TransitionManager.beginDelayedTransition(cvKnowledge, AutoTransition())
+            expandableLayout.visibility = View.VISIBLE
+            ivArrow.setImageResource(R.drawable.ic_arrow_right_24)
+        }
     }
 
     override fun onDestroyView() {
