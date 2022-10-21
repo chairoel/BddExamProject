9b063dff7b5bae2c91abc3b43594d12a6d0bf6c4
chairul <chairul.amri@ebconnect.com>
2022-10-21 14:00:27 +0700

    add splash screen page

---
+++ app/src/main/java/com/amrul/bdd/roseLover/ui/activity/SplashActivity.kt
@@ -0,0 +1,29 @@
+package com.amrul.bdd.roseLover.ui.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import com.amrul.bdd.roseLover.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    lateinit var handler: Handler
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_splash)
+
+        handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
